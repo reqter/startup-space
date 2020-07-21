e61432b4530301403edce046f47980e23b6f0f86
saeedPadyab <padyabsaeed@gmail.com>
2020-07-22 00:20:02 +0300

    html viewer component added

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import Head from "next/head";
-import { withTranslation } from "../../config/Next18Wrapper";
 function Custom404() {
   return <h1>404 - Page Not Found</h1>;
 }
 
-export default withTranslation("common")(Custom404);
+export default Custom404;
