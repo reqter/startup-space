d191a7ecc89119f2699e463bee10de830bb3c518
saeedPadyab <padyabsaeed@gmail.com>
2020-07-25 15:45:52 +0300

    404 page home button api's

--- src/components/404/index.tsx
+++ src/components/404/index.tsx
@@ -3,20 +3,26 @@ import { Link } from "../../../config/Next18Wrapper";
 import { Container, Image, Title, Description, Button } from "./styles";
 import useGlobalState from "hooks/useGlobal/useGlobalState";
 import useObjectPropsValue from "hooks/useObjectPropsValue";
+import useGlobalApi from "hooks/useGlobalApi";
+
 const ContentNotFound = () => {
+  const { notFoundPageInfo, landingData } = useGlobalState();
+  const { getHomeData } = useGlobalApi();
   const { includeImageBaseUrl, getValue } = useObjectPropsValue();
-  const { notFoundPageInfo } = useGlobalState();
   const img =
     notFoundPageInfo && notFoundPageInfo.image
       ? includeImageBaseUrl(notFoundPageInfo.image[0], "image", 500, 500)
       : "";
+  function handleClick() {
+    if (!landingData || landingData.length === 0) getHomeData();
+  }
   return (
     <Section bgColor={theme`colors.gray.100`}>
       <Container>
         <Image src={img} />
         <Title>{getValue(notFoundPageInfo, "title")}</Title>
         <Description>{getValue(notFoundPageInfo, "description")}</Description>
-        <Button>
+        <Button onClick={handleClick}>
           <Link href={"/"}>
             {getValue(notFoundPageInfo, "actionbuttontext")}
           </Link>
